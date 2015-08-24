31a39c09437065b17e877958c707ee34391e401e
David Arvelo <david@davidarvelo.com>
2015-08-24 01:41:05 -0400

    Create JSON endpoints to serve data from database

--- urls.py
+++ urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
-from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^data/organizations.json$', RedirectView.as_view(url='/static/search/data/organizations.json', permanent=False)),
-    url(r'^data/projects.json$', RedirectView.as_view(url='/static/search/data/projects.json', permanent=False)),
+    url(r'^api/organizations$', views.organizations, name='organizations'),
+    url(r'^api/projects$', views.projects, name='projects'),
 ]
--- views.py
+++ views.py
@@ -2,6 +2,8 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Organization, Project
+
 def index(request):
     context = {}
 
@@ -9,3 +11,11 @@ def index(request):
         context['search_query'] = request.GET['query']
 
     return render(request, 'search/index.htmldjango', context)
+
+def organizations(request):
+    data = json.dumps([org.toJSON() for org in Organization.objects.all()])
+    return HttpResponse(data, content_type='application/json')
+
+def projects(request):
+    data = json.dumps([proj.toJSON() for proj in Project.objects.all()])
+    return HttpResponse(data, content_type='application/json')
