72f41c1771e0033a469322e95004d6de00cb6c75
David Arvelo <david@davidarvelo.com>
2015-08-24 20:35:32 -0400

    Abstract the json bootstrapping algorithm

--- bootstrap.py
+++ bootstrap.py
@@ -1,80 +1,52 @@
 import os
 import json
 
+from . import json_loader
 from .models import *
 
-# see if you can make this module run *after* the server is started, with some kinda django hook
-# then query the GitHub API as much as you can for the *most stale* data
-# (sort data by most stale first and iterate until you run out of rate limit)
-# take a second in between requests to not flood GitHub's servers
-
-organizations = json.load(open(os.path.join(os.path.dirname(__file__), 'frontend/organizations.json')))
-projects = json.load(open(os.path.join(os.path.dirname(__file__), 'frontend/projects.json')))
-
-saved_categories = {cat.name: cat for cat in Category.objects.all()}
-saved_tags = {tag.name: tag for tag in Tag.objects.all()}
-
-def fill_auxiliary(Manager, saved, data, key):
-    """
-    gather all categories/tags, then fill the database with any new ones
-    """
-    unsaved = set()
-
-    for d in data:
-        unsaved.update(d[key])
-
-    unsaved = unsaved - set(saved.keys())
-
-    for name in unsaved:
-        saved[name] = Manager.objects.create(name=name)
-
-def fill_organizations():
-    """
-    create all groups with the appropriate categories
-    """
-    for o in organizations:
-        try:
-            org = Organization.objects.get(pk=o['id'])
-        except:
-            org = Organization()
-
-        # update properties
-        org.name = o['name']
-        org.description = o['description']
-        org.homepage    = o.get('homepage', '')
-        org.github_url  = o.get('github_url', '')
-
-        # save org before we start modifying its ManyToManyField
-        # ref: https://docs.djangoproject.com/en/1.8/topics/db/examples/many_to_many/
-        org.save()
-        org.categories.add(*(saved_categories[cat] for cat in o['categories']))
-
-def fill_projects():
-    """
-    create all projects with the appropriate tags
-    """
-    for p in projects:
-        try:
-            proj = Project.objects.get(pk=p['id'])
-        except:
-            proj = Project()
-
-        # update properties
-        proj.name = p['name']
-        proj.description = p['description']
-        proj.homepage    = p.get('homepage', '')
-        proj.github_url  = p.get('github_url', '')
-
-        if p['organizationId']:
-            proj.organization = Organization.objects.get(pk=p['organizationId'])
-
-        # save org before we start modifying its ManyToManyField
-        # ref: https://docs.djangoproject.com/en/1.8/topics/db/examples/many_to_many/
-        proj.save()
-        proj.tags.add(*(saved_tags[tag] for tag in p['tags']))
-
 # load json into database, updating records where needed
-fill_auxiliary(Category, saved_categories, organizations, 'categories')
-fill_auxiliary(Tag, saved_tags, projects, 'tags')
-fill_organizations()
-fill_projects()
+orgs_json = json.load(open(os.path.join(
+    os.path.dirname(__file__),
+    'frontend/organizations.json')
+))
+projects_json = json.load(open(os.path.join(
+    os.path.dirname(__file__),
+    'frontend/projects.json')
+))
+json_loader.fill_auxiliary(
+    manager=Category,
+    field='name',
+    json=orgs_json,
+    json_key='categories'
+)
+json_loader.fill_auxiliary(
+    manager=Tag,
+    field='name',
+    json=projects_json,
+    json_key='tags'
+)
+json_loader.fill_models(
+    manager=Organization,
+    pk='id',
+    json=orgs_json,
+    attrs=(
+        'name',
+        'description',
+        {'name': 'homepage',   'default': ''},
+        {'name': 'github_url', 'default': ''},
+        {'name': 'categories', 'many_to_many': True, 'manager': Category, 'map_field': 'name'}
+    ),
+)
+json_loader.fill_models(
+    manager=Project,
+    pk='id',
+    json=projects_json,
+    attrs=(
+        'name',
+        'description',
+        {'name': 'homepage',   'default': ''},
+        {'name': 'github_url', 'default': ''},
+        {'name': 'tags', 'many_to_many': True, 'manager': Tag, 'map_field': 'name'},
+        {'name': 'organization', 'foreign_key': True, 'manager': Organization, 'json_key': 'organizationId'}
+    ),
+)
---
+++ json_loader.py
@@ -0,0 +1,74 @@
+def fill_auxiliary(manager, field, json, json_key):
+    """
+    gather all categories/tags, then fill the database with any new ones
+    """
+    saved = set(getattr(o, field) for o in manager.objects.all())
+    unsaved = set()
+
+    for obj in json:
+        unsaved.update(obj[json_key])
+
+    # get a set of all models not yet saved
+    unsaved = unsaved - saved
+
+    # create new models and save them
+    for name in unsaved:
+        model = manager()
+        setattr(model, field, name)
+        model.save()
+
+def fill_models(manager, pk, json, attrs):
+    """
+    create/update all models with the appropriate fields
+    """
+    for obj in json:
+        try:
+            model = manager.objects.get(pk=obj[pk])
+        except:
+            model = manager()
+
+        normal_attrs = []
+        default_attrs = []
+        foreign_key_attrs = []
+        many_to_many_attrs = []
+
+        # update properties
+        for attr in attrs:
+            if type(attr) == str:
+                normal_attrs.append(attr)
+
+            if type(attr) == dict:
+                if 'default' in attr:
+                    default_attrs.append(attr)
+                elif 'foreign_key' in attr:
+                    foreign_key_attrs.append(attr)
+                elif 'many_to_many' in attr:
+                    many_to_many_attrs.append(attr)
+
+        for attr in normal_attrs:
+            setattr(model, attr, obj[attr])
+
+        for attr in default_attrs:
+            setattr(model, attr['name'], obj.get(attr['name'], attr['default']))
+
+        for attr in foreign_key_attrs:
+            name = attr['name']
+            json_key = attr['json_key']
+            foreign_manager = attr['manager']
+            if obj[json_key]:
+                setattr(model, name, foreign_manager.objects.get(pk=obj[json_key]))
+
+        # save model before we start modifying its ManyToManyFields
+        # ref: https://docs.djangoproject.com/en/1.8/topics/db/examples/many_to_many/
+        model.save()
+
+        # map the foreign table by the given field
+        for attr in many_to_many_attrs:
+            name = attr['name']
+            foreign_manager = attr['manager']
+            field = attr['map_field']
+            foreign_set = getattr(model, name)
+            # set up reverse lookup for foreign objects so that
+            # we can get the foreign object by its key in the json array
+            foreign_objs = {getattr(o, field): o for o in foreign_manager.objects.all()}
+            foreign_set.add(*(foreign_objs[key] for key in obj[name]))
