b476a2d22efb5a6488eee95d67c352f2f6bd0c83
David Arvelo <david@davidarvelo.com>
2015-08-30 02:32:22 -0400

    Update bootstrap manage.py command

--- management/commands/bootstrap.py
+++ management/commands/bootstrap.py
@@ -1,70 +1,71 @@
 import os
 import json
+from datetime import datetime
 
 from django.core.management.base import BaseCommand, CommandError
 
-from search.models import *
+from search import models
 from . import _json_loader as json_loader
 
 class Command(BaseCommand):
     help = 'Loads json data from specific locations into the database.'
 
-    def add_arguments(self, parser):
-        pass
-
     def handle(self, *args, **options):
         # load json into database, updating records where needed
-        orgs_json = json.load(open(os.path.join(
-            os.path.dirname(__file__),
-            '../..',
-            'frontend/organizations.json')
-        ))
-        projects_json = json.load(open(os.path.join(
-            os.path.dirname(__file__),
-            '../..',
-            'frontend/projects.json')
-        ))
-        json_loader.fill_auxiliary(
-            manager=Category,
-            field='name',
-            json=orgs_json,
-            json_key='categories'
-        )
-        json_loader.fill_auxiliary(
-            manager=Tag,
-            field='name',
-            json=projects_json,
-            json_key='tags'
-        )
-        json_loader.fill_auxiliary(
-            manager=License,
-            field='name',
-            json=projects_json,
-            json_key='license'
-        )
-        json_loader.fill_models(
-            manager=Organization,
-            pk='id',
-            json=orgs_json,
-            attrs=(
-                'name',
-                'description',
-                {'name': 'homepage',   'default': ''},
-                {'name': 'categories', 'many_to_many': True, 'manager': Category, 'map_field': 'name'},
-                {'name': 'github_path', 'default': ''},
-            ),
-        )
-        json_loader.fill_models(
-            manager=Project,
-            pk='id',
-            json=projects_json,
-            attrs=(
-                'name',
-                'description',
-                {'name': 'homepage',   'default': ''},
-                {'name': 'license', 'foreign_key_unique_name': True, 'manager': License, 'json_key': 'license'},
-                {'name': 'organization', 'foreign_key_id': True, 'manager': Organization, 'json_key': 'organizationId'},
-                {'name': 'tags', 'many_to_many': True, 'manager': Tag, 'map_field': 'name'},
-                {'name': 'github_path', 'default': ''},
-            ),
-        )
+        try:
+            orgs_json = json.load(open(os.path.join(
+                os.path.dirname(__file__),
+                '../..',
+                'frontend/organizations.json')
+            ))
+            projects_json = json.load(open(os.path.join(
+                os.path.dirname(__file__),
+                '../..',
+                'frontend/projects.json')
+            ))
+            json_loader.fill_auxiliary(
+                manager=models.Category,
+                field='name',
+                json=orgs_json,
+                json_key='categories'
+            )
+            json_loader.fill_auxiliary(
+                manager=models.Tag,
+                field='name',
+                json=projects_json,
+                json_key='tags'
+            )
+            json_loader.fill_auxiliary(
+                manager=models.License,
+                field='name',
+                json=projects_json,
+                json_key='license'
+            )
+            json_loader.fill_models(
+                manager=models.Organization,
+                pk='id',
+                json=orgs_json,
+                attrs=(
+                    'name',
+                    'description',
+                    {'name': 'homepage',   'default': ''},
+                    {'name': 'categories', 'many_to_many': True, 'manager': models.Category, 'map_field': 'name'},
+                    {'name': 'github_path', 'default': ''},
+                ),
+            )
+            json_loader.fill_models(
+                manager=models.Project,
+                pk='id',
+                json=projects_json,
+                attrs=(
+                    'name',
+                    'description',
+                    {'name': 'homepage',   'default': ''},
+                    {'name': 'license', 'foreign_key_unique_name': True, 'manager': models.License, 'json_key': 'license'},
+                    {'name': 'organization', 'foreign_key_id': True, 'manager': models.Organization, 'json_key': 'organizationId'},
+                    {'name': 'tags', 'many_to_many': True, 'manager': models.Tag, 'map_field': 'name'},
+                    {'name': 'github_path', 'default': ''},
+                ),
+            )
+        except:
+            raise CommandError('Bootstrapping from JSON failed. Not all data was loaded.')
