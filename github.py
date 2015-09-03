940713f12ce66bb3f97ccf0a1faf7290b6d17031
David Arvelo <david@davidarvelo.com>
2015-09-02 23:32:44 -0400

    Advance GitHubHeartbeat caching strategy

--- github.py
+++ github.py
@@ -1,16 +1,19 @@
 import json
+import requests
 from queue import PriorityQueue
 from urllib.request import urlopen
 from threading import Thread, Timer
-from datetime import datetime
-from .models import Organization, Project, GitHubCache as GithubCacheModel
+from datetime import datetime, timezone
+from .models import Organization, Project, GitHubOrganizationCache, GitHubProjectCache, ProgrammingLanguage
 
 class GitHubHeartbeat():
     priority_uncached = 0
     priority_user_requested = 10
     priority_normal = 20
 
+    stale_threshold = 900
     interval_check_database = 900
+
     # @TODO: change this to `1` in production
     interval_uncached = 5
     interval_user_requested = 30
@@ -40,26 +43,48 @@ class GitHubHeartbeat():
         self.check_database_timer = None
         self.fetch_timer = None
 
-    def queue_items(items, user_requested=False):
+    def decrement_rate_limit_remaining(self):
+        self.rate_limit['remaining'] -= 1
+
+    def queue_items(self, items, data_type, user_requested=False):
+        priority_normal = self.priority_normal
+        priority_uncached = self.priority_uncached
+        priority_user_requested = self.priority_user_requested
+        stale_threshold = self.stale_threshold
+
+        if data_type == 'org':
+            cache_manager = GitHubOrganizationCache
+        elif data_type == 'project':
+            cache_manager = GitHubProjectCache
+        else:
+            return
+
         for item in items:
             path = item.github_path
 
-            # always put highest priority on uncached items.
-            # make sure not to queue uncached items twice if they're queued again during the timer delay
-            if item.github_data == None and path not in self.queued:
-                self.queued[path] = priority_uncached
-                self.queue.put((priority_uncached, path))
-
-            # queue the item as user-requested only if it's not in the queue, or is, but with lower priority
-            elif user_requested and (path not in self.queued or self.queued[path] > priority_user_requested):
-                self.queued[path] = priority_user_requested
-                self.queue.put((priority_user_requested, path))
-
-            else:
+            if not path in self.queued:
+                # always queue every item once with normal priority so that
+                # it can be fetched on rotation in order to keep the data fresh
+                print('Queueing normal priority for url:', path)
                 self.queued[path] = priority_normal
                 self.queue.put((priority_normal, path))
 
-    def enqueue(self, manager, github_paths):
+            current_priority = self.queued[path]
+
+            # always put highest priority on uncached items.
+            # only queue items as uncached once during the life of the server
+            if item.github_data == None and current_priority > priority_uncached:
+                print('Queueing uncached priority for url:', path)
+                self.queued[path] = priority_uncached
+                self.queue.put((priority_uncached, path))
+            # only queue items as user_requested once during the life of the server
+            elif user_requested and self.queued[path] > priority_user_requested:
+                if item.github_data and (datetime.now() - item.github_data.fetched).total_seconds() > stale_threshold:
+                    print('Queueing user requested priority for url:', path)
+                    self.queued[path] = priority_user_requested
+                    self.queue.put((priority_user_requested, path))
+
+    def enqueue(self, manager, github_paths, data_type):
         """
         public interface for user-requested github paths through the API
         """
@@ -67,8 +92,7 @@ class GitHubHeartbeat():
             github_paths = (github_paths,)
 
         items = manager.objects.filter(github_path__in=github_paths)
-        # @TODO: need to differentiate between Orgs and Projects to do DB query
-        self.queue_items(items, user_requested=True)
+        self.queue_items(items, data_type, user_requested=True)
 
     def check_database(self):
         """
@@ -77,49 +101,133 @@ class GitHubHeartbeat():
         orgs = Organization.objects.filter(github_path__isnull=False)
         projects = Project.objects.filter(github_path__isnull=False)
 
-        self.queue_items(orgs)
-        self.queue_items(projects)
+        self.queue_items(orgs, data_type='org')
+        self.queue_items(projects, data_type='project')
 
         self.check_database_timer = Timer(self.interval_check_database, self.check_database)
         self.check_database_timer.start()
 
-    def fetch_next(self, github_paths):
+    class Downloader(Thread):
+        org_url = 'https://api.github.com/orgs/%s'
+        project_url = 'https://api.github.com/repos/%s'
+        headers = {'accept': 'application/vnd.github.v3+json'}
+
+        def __init__(self, heartbeat, github_path, priority, *args, **kwargs):
+            Thread.__init__(self, *args, **kwargs)
+            self.heartbeat = heartbeat
+            self.github_path = github_path
+            self.priority = priority
+
+        def run(self):
+            if '/' not in self.github_path:
+                url = self.org_url % self.github_path
+                manager = Organization
+            else:
+                url = self.project_url % self.github_path
+                manager = Project
+
+            try:
+                model = manager.objects.get(github_path=self.github_path)
+            except:
+                # If the model no longer exists, don't try to fetch it and don't requeue it
+                self.heartbeat.queue.task_done()
+                return
+
+            print('Getting GitHub API url:', url, 'with priority', self.priority)
+            self.heartbeat.decrement_rate_limit_remaining()
+            response = requests.get(url, headers=self.headers)
+
+            if response.status_code == 200:
+                if manager == Organization:
+                    print('Filling organization with data:', response.text)
+                    self.fill_org_data(model, response)
+                else:
+                    print('Filling project with data:', response.text)
+                    self.fill_project_data(model, response)
+            else:
+                # @TODO: if not a 200 status code, send an email with the details
+                print('GitHub API Error getting url:', url)
+                print('Response status code was:', response.status_code)
+                print('Error message was:', response.text)
+
+            if response.status_code != 404:
+                self.requeue()
+
+            self.heartbeat.queue.task_done()
+
+        def fill_org_data(self, model, response):
+            try:
+                cache = GitHubOrganizationCache.objects.get(github_path=self.github_path)
+            except:
+                cache = GitHubOrganizationCache(github_path=self.github_path)
+
+            data = response.json()
+
+            cache.fetched = datetime.now()
+            cache.json = json.dumps(data)
+            cache.save()
+
+            model.github_data = cache
+            model.save()
+
+        def fill_project_data(self, model, response):
+            try:
+                cache = GitHubProjectCache.objects.get(github_path=self.github_path)
+            except:
+                cache = GitHubProjectCache(github_path=self.github_path)
+
+            data = response.json()
+            # @TODO: If you are using MySQL, be sure to use the READ COMMITTED isolation level rather than REPEATABLE READ (the default), otherwise you may see cases where get_or_create will raise an IntegrityError but the object won’t appear in a subsequent get() call.
+            # ref: https://docs.djangoproject.com/en/dev/ref/models/querysets/#get-or-create-kwargs
+            language, was_created = ProgrammingLanguage.objects.get_or_create(name=data['language'])
+
+            try:
+                last_commit_date = datetime.fromtimestamp(data['pushed_at'])
+            except:
+                last_commit_date = None
+
+            cache.fetched = datetime.now()
+            cache.json = json.dumps(data)
+            cache.open_issues_count = data['open_issues_count']
+            cache.stargazers_count = data['stargazers_count']
+            cache.last_commit = last_commit_date
+            cache.language = language
+            cache.save()
+
+            model.github_data = cache
+            model.save()
+
+        def requeue(self):
+            if self.priority < self.heartbeat.priority_normal:
+                # reset priority for that github_path in case it needs to be elevated again by a user request
+                self.heartbeat.queued[self.github_path] = self.heartbeat.priority_normal
+                # don't requeue because a normal_priority task is automatically queued for all items
+                return
+
+            # automatically requeue normal_priority items.
+            # this keeps them on rotation so the cache data stays fresh
+            self.queue.put((self.heartbeat.priority_normal, url))
+
+    def fetch_next(self):
         """
         pop the next github path from the queue and fetch it using the GitHub API
         """
-        # if user-requested, remove it from the queue when done since it's already got a normal_priority instance to be recycled in the queue.
-        def worker():
-            # use a try block in case the model's been deleted during the timer wait
-            # add any new Languages to ProgrammingLanguage Model and link them to GitHubCache foreign key
-            # GitHubCache has its own db fields for issue_count etc so the client can use the API to query them
-            # hook the GitHubCache back up with its original requester Organization/Project
-            #   maybe should use the 'type' prop in the returned JSON
-            #   maybe should just expand the data held by queue to be a dictionary
-
-            # url: `https://api.github.com/repos/${this.owner}/${this.repo}`,
-            # headers: {
-            #     'Accept': 'application/vnd.github.v3+json'
-            # },
-            # json.loads(str, ensure_ascii=False)
-            pass
-
-        if rate_limit['remaining'] <= 0:
+        if self.rate_limit['remaining'] <= 0:
             interval = self.rate_limit['time_left']
         else:
-            # get a task from the queue
-            # download GitHub data for the item and store it in the database
-            self.rate_limit['remaining'] -= 1
-            url = self.queue.pop()[1]
-            Thread(target=worker, args=(self.queue, url))
+            priority, github_path = self.queue.get()
+
+            # Spawn a thread to download the GitHub data for the item and store it in the database
+            self.Downloader(self, github_path, priority).start()
 
             # set timer for getting the next task
-            next_task = self.queue.pop()
+            next_task = self.queue.get()
             next_priority = next_task[0]
             self.queue.put(next_task)
 
-            if next_priority == priority_uncached:
+            if next_priority == self.priority_uncached:
                 interval = self.interval_uncached
-            elif next_priority == priority_user_requested:
+            elif next_priority == self.priority_user_requested:
                 interval = self.interval_user_requested
             else:
                 interval = self.interval_normal
