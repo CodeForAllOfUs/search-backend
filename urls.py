5d29d1192049b31c7499f019a9e37e40a6a9ca80
David Arvelo <david@davidarvelo.com>
2015-09-07 23:23:05 -0400

    Start separating out GitHub cache endpoints

--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/organizations$', views.organizations, name='organizations'),
     url(r'^api/projects$', views.projects, name='projects'),
-    url(r'^api/github-data$', views.github_data, name='github_data'),
+    url(r'^api/github-data/organizations$', views.github_data_orgs, name='github_data_orgs'),
+    url(r'^api/github-data/projects$', views.github_data_projects, name='github_data_projects'),
 ]
--- views.py
+++ views.py
@@ -33,25 +33,42 @@ def projects(request):
 # @TODO: cache all JSON for at least 15 minutes in production
 #        @cache_control(max_age=900)
 @cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
-def github_data(request):
+def github_data_orgs(request):
     try:
-        data_type = request.GET['type']
         github_paths = set(parse_qs(request.GET.urlencode())['github_paths[]'])
-
-        if data_type == 'organizations':
-            manager = Organization
-            cache_manager = GitHubOrganizationCache
-        elif data_type == 'projects':
-            manager = Project
-            cache_manager = GitHubProjectCache
-        else:
-            raise Exception()
     except:
         return HttpResponseBadRequest()
 
-    cached_paths = [item.toJSON() for item in cache_manager.objects.filter(github_path__in=github_paths)]
+    cached_paths = [item.toJSON() for item in GitHubOrganizationCache.objects.filter(github_path__in=github_paths)]
 
-    gh_heartbeat.enqueue(manager, github_paths)
+    gh_heartbeat.enqueue(Organization, github_paths)
+
+    rate_limit = gh_heartbeat.rate_limit
+    rate_limit = {
+        'remaining': rate_limit['remaining'],
+        'reset_date': rate_limit['reset_date'].isoformat(),
+    }
+
+    # only send data for the paths already in the cache since fetching the
+    # others using the GitHub API will take too long, because of the rate limit.
+    # hopefully they'll be in the cache the next time they're requested.
+    return JsonResponse({
+        'rate_limit':  rate_limit,
+        'github_data': cached_paths,
+    })
+
+# @TODO: cache all JSON for at least 15 minutes in production
+#        @cache_control(max_age=900)
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
+def github_data_projects(request):
+    try:
+        github_paths = set(parse_qs(request.GET.urlencode())['github_paths[]'])
+    except:
+        return HttpResponseBadRequest()
+
+    cached_paths = [item.toJSON() for item in GitHubProjectCache.objects.filter(github_path__in=github_paths)]
+
+    gh_heartbeat.enqueue(Project, github_paths)
 
     rate_limit = gh_heartbeat.rate_limit
     rate_limit = {
