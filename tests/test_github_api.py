2c542cc1ae4331dd25d67852734f759b5988f555
David Arvelo <david@davidarvelo.com>
2015-10-27 17:51:27 -0400

    update test

--- tests/test_github_api.py
+++ tests/test_github_api.py
@@ -3,21 +3,15 @@ from django.core.urlresolvers import reverse
 
 class IndexViewTests(TestCase):
     def test_index_view_with_no_query_params(self):
-        """
-        If no questions exist, an appropriate message should be displayed.
-        """
         response = self.client.get(reverse('search:index'))
         self.assertTemplateUsed(response, 'search/index.htmldjango')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<input id="searchbar" type="search" placeholder="Search" class="form-control input-lg" />', html=True)
 
-    def test_index_view_with_search_query_params(self):
-        """
-        If no questions exist, an appropriate message should be displayed.
-        """
+    def test_index_view_with_search_query_param(self):
         context = {'query': 'test search'}
         response = self.client.get(reverse('search:index'), context)
         self.assertTemplateUsed(response, 'search/index.htmldjango')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context['search_query'], context['query'])
-        self.assertContains(response, '<input id="searchbar" type="search" value="test search" placeholder="Search" class="form-control input-lg" />', html=True)
+        self.assertContains(response, '<input id="searchbar" type="search" value="{}" placeholder="Search" class="form-control input-lg" />'.format(context['query']), html=True)
