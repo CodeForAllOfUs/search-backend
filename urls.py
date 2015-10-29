f19942c41b5f45b98baacfd42213ae96e6abd79d
David Arvelo <david@davidarvelo.com>
2015-10-28 23:00:34 -0400

    Change API endpoints

--- urls.py
+++ urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 
-from . import views
+import search.views as views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/organizations$', views.organizations, name='organizations'),
+    url(r'^api/organizations/github-data$', views.github_data_orgs, name='github_data_orgs'),
     url(r'^api/projects$', views.projects, name='projects'),
-    url(r'^api/github-data/organizations$', views.github_data_orgs, name='github_data_orgs'),
-    url(r'^api/github-data/projects$', views.github_data_projects, name='github_data_projects'),
+    url(r'^api/projects/github-data$', views.github_data_projects, name='github_data_projects'),
 ]
