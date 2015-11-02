ba4874a7893d41db2bf810242187594601bcc49d
David Arvelo <david@davidarvelo.com>
2015-11-01 23:32:46 -0500

    Fix setup.py classifiers

--- setup.py
+++ setup.py
@@ -15,13 +15,15 @@ setup(
     ''',
     license='MIT',
     classifiers=[
-        'Operating System :: Unix',
-        'Operating System :: POSIX :: Linux',
+        'Environment :: Web Environment',
+        'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ]
 )
